a829a7c92c8e4bff8af386600fcf161b10016562
Dusan-O <dusan.orescanin@gmail.com>
2022-08-21 19:10:04 +0200

    last app version v.1.0.1 Resolve errors No erros

--- CoreMLApp/ViewController.swift
+++ CoreMLApp/ViewController.swift
@@ -7,15 +7,14 @@
 
 import UIKit
 import PhotosUI
+import Vision
 
 class ViewController: UIViewController, PHPickerViewControllerDelegate {
-
     
     @IBOutlet weak var predictionLabel: UILabel!
     @IBOutlet weak var imageToAnalyze: UIImageView!
     
     var picker: PHPickerViewController?
-    
     
     
     override func viewDidLoad() {
@@ -37,24 +36,92 @@ class ViewController: UIViewController, PHPickerViewControllerDelegate {
         guard let result = results.first else { return }
         let item = result.itemProvider
         guard item.canLoadObject(ofClass: UIImage.self) else { return }
-        item.loadObject(ofClass: UIImage.self) { bridgeble, error in
+        item.loadObject(ofClass: UIImage.self) { bridgeable, error in
             if let e = error {
-                print("We have an error :\(e.localizedDescription)")
+                print("Nous avons une erreur: \(e.localizedDescription)")
             }
-            if let newImage = bridgeble as? UIImage {
+            if let newImage = bridgeable as? UIImage {
                 DispatchQueue.main.async {
                     self.imageToAnalyze.image = newImage
                 }
             }
         }
     }
-
+    
+    
+    func getCoreML() -> VNCoreMLModel? {
+        let configuration: MLModelConfiguration = MLModelConfiguration()
+        do {
+            let ml = try SqueezeNet(configuration: configuration)
+            let model = try VNCoreMLModel(for: ml.model)
+            return model
+        } catch {
+            print(error.localizedDescription)
+            return nil
+        }
+        
+    }
+    
+    func getCiImage() -> CIImage? {
+        if let image = imageToAnalyze.image {
+            if let ciImage = CIImage(image: image) {
+                //2. Convertir image
+                return ciImage
+            }
+            print("Ci ne fonctionne pas")
+            return nil
+        }
+        print("No image")
+        return nil
+    }
+    
+    func setupRequest(_ model: VNCoreMLModel) -> VNCoreMLRequest {
+        return VNCoreMLRequest(model: model, completionHandler: setupResultHandler(_:_:))
+    }
+    
+    func setupResultHandler(_ vnRequest: VNRequest, _ error: Error?){
+        //5. Obtenir les résultats.
+        let results = vnRequest.results as? [VNClassificationObservation]
+        if let bestMatch = results?.first {
+            let string = "Je suis certain à \(Int(bestMatch.confidence * 100))% que ceci est : \(bestMatch.identifier)"
+            self.updateLabel(string)
+        }
+    }
+    
+    func setupHandler(_ image: CIImage, _ request: VNCoreMLRequest) {
+    //4. Faire cette requete Vision
+    DispatchQueue.global(qos: .userInitiated).async {
+        let handler = VNImageRequestHandler(ciImage: image, options: [:])
+        do {
+            try handler.perform([request])
+        } catch {
+            print("On n'a pas pu faire le perform: \(error.localizedDescription)")
+        }
+    }
+    }
+    
+    func updateLabel(_ string: String) {
+        DispatchQueue.main.async {
+            self.predictionLabel.text = string
+        }
+    }
+        
+    
     @IBAction func addPicture(_ sender: Any) {
         present(picker!, animated: true, completion: nil)
     }
     
     @IBAction func analyzePicture(_ sender: Any) {
+        //1. Soit sur d'avoir une image
+        guard let ciImage = getCiImage() else { return }
+        //3. Mettre en place une requete vision
+        guard let model = getCoreML() else { return}
+        let request = setupRequest(model)
+        setupHandler(ciImage, request)
     }
     
+
+
 }
+
 
